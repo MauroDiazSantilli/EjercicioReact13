eefe141ff7ccaeccaf16064963b88aeb0315c893
MauroDS <mauro.diaz.santilli@gmail.com>
2023-06-04 20:39:48 -0300

    Añadiendo un maquetado inicial

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css'
 import { Container } from 'react-bootstrap';
 import './App.css';
 import Clima from './components/Clima';
 
 function App() {
   return (
+    <>
+    <section className='my-5 main'>
     <Container>
       <Clima />
     </Container>
+    </section>
+      <footer className='bg-dark text-light py-4 text-center'>
+        <p>&copy; Todos los derechos reservados</p>
+      </footer>
+    </>
   );
 }
 
