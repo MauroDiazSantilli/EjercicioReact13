63dde104ffabed90d3d487cb3985ade5ee487db3
MauroDS <mauro.diaz.santilli@gmail.com>
2023-06-04 23:17:23 -0300

    Dando cambios estilisticos a los datos mostrados

--- src/components/ClimaCard.jsx
+++ src/components/ClimaCard.jsx
@@ -5,10 +5,10 @@ const ClimaCard = ({ clima, resultadoFecha }) => {
   return (
     <Card className='mt-5'>
       <Card.Body className='bg-info'>
-        <Card.Title>{clima.name}, {clima.sys.country}</Card.Title>
+        <Card.Title className='text-center'>{clima.name}, {clima.sys.country}</Card.Title>
         <Card.Text>{resultadoFecha(new Date())}</Card.Text>
-        <Card.Text>{Math.round(clima.main.temp)}°C</Card.Text>
-        <Card.Text>{clima.weather[0].main}</Card.Text>
+        <Card.Text className='display-1 text-center'>{Math.round(clima.main.temp)}°C</Card.Text>
+        <Card.Text className='display-5 text-center'>{clima.weather[0].main}</Card.Text>
       </Card.Body>
     </Card>
   );
